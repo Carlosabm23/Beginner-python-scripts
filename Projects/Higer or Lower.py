e53bd4c3635d59122090d4b14a72fc0fd77ef4e4
Carlos B <Carlosabm23@yahoo.com>
2025-04-23 20:21:56 -0400

    commit

---
+++ Projects/Higer or Lower.py
@@ -0,0 +1,49 @@
+import art
+import random
+import game_data
+
+print(art.logo_2)
+#
+#
+# def score_(user_answer,correct_answer,score):
+#     if user_answer == correct_answer:
+#         return score -1
+#
+#
+#
+# def game():
+#     score = 1
+#     while score == 1:
+#         print("\n")
+#         number1 = random.randint(0,100)
+#         number2 = random.randint(0,100)
+#         print(f"Which number is bigger {number1} or or {number2} ?")
+#         answer = int(input("Which is Bigger? \n"))
+#         if  number1 > number2 :
+#             correct = number1
+#         elif number2 > number1:
+#             correct = number2
+#         else:
+#             print("You are wrong")
+#
+#         if answer == correct:
+#             print(f"You are correct {answer} is the bigger number")
+#         else:
+#             print(f"Sorry {correct} was the bigger number")
+#         score_(answer,correct,score)
+#         if score == 0:
+#             print("Over")
+#
+# game()
+
+
+
+
+
+
+
+
+
+
+
+

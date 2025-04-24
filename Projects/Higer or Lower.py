cc46ed858b06be95cd2a7e8e3e03758a40c1bac0
Carlos B <Carlosabm23@yahoo.com>
2025-04-23 21:35:16 -0400

    commit

--- Projects/Higer or Lower.py
+++ Projects/Higer or Lower.py
@@ -1,10 +1,96 @@
 import art
 import random
-import game_data
+from game_data import data
 
-print(art.logo_2)
-#
-#
+
+def format_data(account):
+    """Returns a nicely formatted string with account info."""
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    return f"{name}, a {description}, from {country}"
+
+def check_answer(guess, a_followers, b_followers):
+    """Returns True if guess is correct, else False."""
+    if a_followers > b_followers:
+        return guess == 'a'
+    else:
+        return guess == 'b'
+
+# Main game loop
+def game():
+    print(art.logo)
+    score = 0
+    game_should_continue = True
+    account_a = random.choice(data)
+    account_b = random.choice(data)
+
+    # Make sure starting accounts are different
+    while account_a == account_b:
+        account_b = random.choice(data)
+
+    while game_should_continue:
+        account_a = account_b
+        while True:
+            account_b = random.choice(data)
+            if account_b != account_a:
+                break
+
+        print(f"\nCompare A: {format_data(account_a)}")
+        print(art.logo_3)
+        print(f"Against B: {format_data(account_b)}")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+        a_follower_count = account_a["Follower_count"]
+        b_follower_count = account_b["Follower_count"]
+        name_a = account_a["name"]
+        name_b = account_b["name"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+        if is_correct:
+            score += 1
+            print(f"\n✅ You are correct! Current score: {score}")
+        else:
+            print(f"\n❌ Sorry, that's wrong.")
+            print(f"{name_a} had {a_follower_count} followers. {name_b} had {b_follower_count} followers.")
+            print(f"Final score: {score}")
+            game_should_continue = False
+
+game()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # def score_(user_answer,correct_answer,score):
 #     if user_answer == correct_answer:
 #         return score -1
@@ -35,9 +121,6 @@ print(art.logo_2)
 #             print("Over")
 #
 # game()
-
-
-
 
 
 
